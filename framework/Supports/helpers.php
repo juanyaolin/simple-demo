c73a9662946689ef884a3f754daec7860fa013ef
juanyaolin <maple96541@gmail.com>
2024-05-02 22:44:44 +0800

    feat: requirement-240422

--- framework/Supports/helpers.php
+++ framework/Supports/helpers.php
@@ -40,3 +40,97 @@ if (function_exists('value')) {
         return $value instanceof Closure ? $value(...$args) : $value;
     }
 }
+
+if (!function_exists('base_path')) {
+    /**
+     * Get the path to the base of the install.
+     */
+    function base_path(string $path = ''): string
+    {
+        return dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . ltrim($path, '/\\');
+    }
+}
+
+if (!function_exists('get_request_query')) {
+    /**
+     * Retrieve a query string item from the request.
+     */
+    function get_request_query(
+        ?string $key = null,
+        mixed $default = null
+    ): mixed {
+        return data_get($_GET, $key, $default);
+    }
+}
+
+if (!function_exists('get_request_post')) {
+    /**
+     * Retrieve a request payload item from the request.
+     */
+    function get_request_post(
+        ?string $key = null,
+        mixed $default = null
+    ): mixed {
+        $data = str_contains(data_get($_SERVER, 'CONTENT_TYPE', ''), 'json')
+            ? json_decode(file_get_contents('php://input'), true)
+            : $_POST;
+
+        return data_get($data, $key, $default);
+    }
+}
+
+if (!function_exists('get_request_input')) {
+    /**
+     * Retrieve an input item from the request.
+     */
+    function get_request_input(
+        ?string $key = null,
+        mixed $default = null
+    ): mixed {
+        $data = get_request_post() + get_request_query();
+
+        return data_get($data, $key, $default);
+    }
+}
+
+if (!function_exists('get_request_file')) {
+    /**
+     * Retrieve a file from the request.
+     */
+    function get_request_file(
+        ?string $key = null,
+        mixed $default = null
+    ): ?array {
+        return data_get($_FILES, $key, $default);
+    }
+}
+
+if (!function_exists('get_request_all')) {
+    /**
+     * Get all of the input and files for the request.
+     */
+    function get_request_all(
+        ?string $key = null,
+        mixed $default = null
+    ): ?array {
+        $data = get_request_input() + get_request_file();
+
+        return data_get($data, $key, $default);
+    }
+}
+
+if (!function_exists('beautify_throwable')) {
+    /**
+     * Convert throwable to an human-understandable array structure.
+     */
+    function beautify_throwable(Throwable $throwable): array
+    {
+        return [
+            'message' => $throwable->getMessage(),
+            'throwable' => get_class($throwable),
+            'file' => $throwable->getFile(),
+            'line' => $throwable->getLine(),
+            'trace' => $throwable->getTrace(),
+        ];
+    }
+}
---
+++ public/todo_session.php
@@ -0,0 +1,137 @@
+<?php
+declare(strict_types = 1);
+
+require_once __DIR__ . '../../../../vendor/autoload.php';
+
+session_start();
+
+$todos = $_SESSION['todos'] ?? [];
+$now = time();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (strlen($_POST['add'] ?? '') > 0) {
+        $todos[] = [
+            'id' => uniqid(),
+            'content' => $_POST['add'],
+            'enabled' => true,
+            'createdAt' => $now,
+            'updatedAt' => $now,
+            'deletedAt' => null,
+        ];
+    }
+
+    if (isset($_POST['edit'])) {
+        foreach ($todos as $idx => $todo) {
+            if ($todo['id'] === $_POST['edit']) {
+                $todo['content'] = $_POST[$todo['id']];
+                $todo['updatedAt'] = $now;
+
+                $todos[$idx] = $todo;
+            }
+        }
+    }
+
+    if (isset($_POST['toggle'])) {
+        foreach ($todos as $idx => $todo) {
+            if ($todo['id'] === $_POST['toggle']) {
+                $todo['enabled'] = !$todo['enabled'];
+                $todo['updatedAt'] = $now;
+
+                $todos[$idx] = $todo;
+            }
+        }
+    }
+
+    if (isset($_POST['delete'])) {
+        foreach ($todos as $idx => $todo) {
+            if ($todo['id'] === $_POST['delete']) {
+                $todo['deletedAt'] = $now;
+                $todos[$idx] = $todo;
+            }
+        }
+    }
+
+    $_SESSION['todos'] = $todos;
+}
+
+dump($_POST, $todos);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Todo List</h1>
+
+        <!-- add -->
+        <form action="" method="post">
+            <div class="">
+                <input type="text" name="add" autocomplete="off">
+                <button type="submit">新增</button>
+            </div>
+        </form>
+
+        <hr>
+
+        <?php foreach ($todos as $todo) { ?>
+
+            <?php if (!is_null($todo['deletedAt'])) {
+                continue;
+            } ?>
+
+            <div class="" style="margin-bottom: 0.25rem; display:flex">
+
+                <!-- toggle -->
+                <form action="" method="post">
+                    <input
+                        type="hidden"
+                        name="toggle"
+                        value="<?php echo $todo['id']; ?>"
+                        autocomplete="off"
+                    >
+                    <button type="submit">
+                        <?php echo $todo['enabled'] ? '關閉' : '開啟'; ?>
+                    </button>
+                </form>
+
+                <!-- edit -->
+                <form action="" method="post">
+                    <input
+                        type="text"
+                        name="<?php echo $todo['id']; ?>"
+                        value="<?php echo $todo['content']; ?>"
+                        autocomplete="off"
+                        <?php echo $todo['enabled'] ? '' : 'disabled'; ?>
+                    >
+                    <input
+                        type="hidden"
+                        name="edit"
+                        value="<?php echo $todo['id']; ?>"
+                        autocomplete="off"
+                    >
+                    <?php if ($todo['enabled']) { ?>
+                        <button type="submit">儲存</button>
+                    <?php } ?>
+                </form>
+
+                <!-- delete -->
+                <form action="" method="post">
+                    <input
+                        type="hidden"
+                        name="delete"
+                        value="<?php echo $todo['id']; ?>"
+                        autocomplete="off"
+                    >
+                    <button type="submit">刪除</button>
+                </form>
+            </div>
+        <?php } ?>
+
+    </main>
+</body>
+</html>
